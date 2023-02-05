3ceb0fd9e65e60b7d2c714477759835a2123e204
Jairop98 <jairop98@yahoo.com>
2023-02-04 18:24:47 -0800

    first commit

---
+++ LoopBasicI.py
@@ -0,0 +1,28 @@
+for i in range (151):
+    print (i)
+
+for i in range (5, 1001, 5):
+    print (i)
+
+for i in range(0 ,101):
+    if i % 10 == 0:
+        print("Coding Dojo")
+    elif i % 5 == 0:
+        print("Coding")
+    else:
+        print(i)
+
+sum = 0
+for i in range(1,500001,2):
+    sum += i
+print(sum)
+
+for x in range(2018, 0, -4):
+    print(x)
+
+low = 2
+high = 9 
+mult = 3
+
+for i in range(low, high, mult):
+    print(i+1)
